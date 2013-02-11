66bcb985569e7b643823c9ba1aeb47892583aeef
Ingy döt Net <ingy@ingy.net>
2013-02-11 11:25:17 -0800

    spec-1.2 rule [53]
    Support "\/" for JSON compatability.

--- src/scanner.c
+++ src/scanner.c
@@ -3156,6 +3156,10 @@ yaml_parser_scan_flow_scalar(yaml_parser_t *parser, yaml_token_t *token,
                         *(string.pointer++) = '"';
                         break;
 
+                    case '/':
+                        *(string.pointer++) = '/';
+                        break;
+
                     case '\'':
                         *(string.pointer++) = '\'';
                         break;
