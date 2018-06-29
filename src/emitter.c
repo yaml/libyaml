56f4b17221868593d6903ee58d6d679b690cf4df
Tina Müller <cpan2@tinita.de>
2018-06-29 23:16:26 +0200

    Don't emit document-end marker at the end of stream
    (only when explicitly requested)
    
    @jrtc27++ for the patch.
    
    See #60

--- src/emitter.c
+++ src/emitter.c
@@ -649,13 +649,6 @@ yaml_emitter_emit_document_start(yaml_emitter_t *emitter,
 
     else if (event->type == YAML_STREAM_END_EVENT)
     {
-        if (emitter->open_ended)
-        {
-            if (!yaml_emitter_write_indicator(emitter, "...", 1, 0, 0))
-                return 0;
-            if (!yaml_emitter_write_indent(emitter))
-                return 0;
-        }
 
         if (!yaml_emitter_flush(emitter))
             return 0;
