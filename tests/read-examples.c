2b9332b3336d32948648d0609a5dacb34cc11983
Peter Murphy <peterkmurphy@gmail.com>
2019-02-16 15:40:58 +1000

    First read examples stub

---
+++ tests/read-examples.c
@@ -0,0 +1,17 @@
+#include <yaml.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#include <assert.h>
+
+int
+main(void)
+{
+    /* Print structure sizes. */
+    printf("THESE ARE THE EXAMPLE FILES!\n");
+    return 0;
+}
