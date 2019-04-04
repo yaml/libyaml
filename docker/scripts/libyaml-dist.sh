4b53141611c811f7498957ec8c0a0c5aa1c82932
Tina Müller <cpan2@tinita.de>
2019-04-05 01:11:37 +0200

    Add creation of zipfile

--- docker/scripts/libyaml-dist.sh
+++ docker/scripts/libyaml-dist.sh
@@ -3,6 +3,7 @@
 set -e
 set -x
 
+# Create tarball
 cd /tmp
 cp -rp /output/libyaml.git .
 #git clone https://github.com/yaml/libyaml.git
@@ -11,5 +12,15 @@ cd libyaml.git
 ./configure
 make dist
 
-cp yaml-*.tar.gz /output
+# get the tarball filename
+tarball="$(ls yaml-*.tar.gz | head -1)"
+dirname="${tarball/.tar.gz/}"
 
+# Copy to output dir
+cp "$tarball" /output
+
+# Create zip archive
+cd /tmp
+cp "/output/$tarball" .
+tar xvf "$tarball"
+zip -r "/output/$dirname.zip" "$dirname"
